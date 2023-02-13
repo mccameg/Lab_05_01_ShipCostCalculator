21c0d5e0ce6610391780900b6d2407bc70dcc000
mccameg <meganmccarren@yahoo.com>
2023-02-12 19:16:10 -0500

    Final

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,14 @@ public class Main {
 	double shippingCost = 0;
 	double totalCost = 0;
         if (itemCost < FREE_SHIP)
-        {   shippingCost = itemCost * .02;
-            totalCost = itemCost + shippingCost;
-            System.out.println("The shipping cost is $" + shippingCost + ". The total cost is $" + totalCost);
+        {   shippingCost = itemCost * .02; // if the item cost is over $100, then we will calculate the shipping cost to be 2% of the item cost and store the shipping cost
+            totalCost = itemCost + shippingCost; // if the item cost is over $100, then we find the total cost by adding the shipping cost to the item cost
+            System.out.println("The shipping cost is $" + shippingCost + ". The total cost is $" + totalCost); // outputs the shipping cost and total cost if the item cost is over $100
         }
-        else
-
-        System.out.println("Shipping is free. The total cost is $" + itemCost);
+        else // item cost is >= $100
+        {
+            System.out.println("Shipping is free. The total cost is $" + itemCost); // if item cost is $100 or more, then the output tells us that shipping is free and outputs the total cost at the cost of the item alone
+        }
     }
 
 }
